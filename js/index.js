70bb18cdadb56df629f65d59add72c53f300b2d6
gadpro <101449500+gadpro@users.noreply.github.com>
2023-04-22 14:15:53 +0800

    新彩蛋

--- js/index.js
+++ js/index.js
@@ -7,27 +7,31 @@ $(function () {
             }
             else{
                 if(url_value == 'homo'){
-                    const audioh = new Audio('./audio/homo.mp3')
+                    const audioh = new Audio('audio\\homo.mp3')
                     audioh.play()
                 }
                 else if(url_value == 'rickroll' || url_value == 'rickastley'){
                     window.open('https://www.bilibili.com/video/BV1GJ411x7h7')
                 }
                 else if(url_value == 'spasmodic'){
-                    const audios = new Audio('./audio/spasmodic.mp3')
+                    const audios = new Audio('audio\\spasmodic.mp3')
                     audios.play()
                 }
                 else if(url_value == 'chicken'){
-                    const audioc = new Audio('./audio/chicken.mp3')
+                    const audioc = new Audio('audio\\chicken.mp3')
                     audioc.play()
                 }
                 else if(url_value == 'random'){
-                    const audior = new Audio('./audio/random.mp3')
+                    const audior = new Audio('audio\\random.mp3')
                     audior.play()
                 }
                 else if(url_value == 'again'){
-                    const audioa = new Audio('./audio/again.mp3')
+                    const audioa = new Audio('audio\\again.mp3')
                     audioa.play()
+                }
+                else if(url_value == 'one'){
+                    const audioo = new Audio('audio\\one.mp3')
+                    audioo.play()
                 }
                 else{
                     window.open('https://game.gadpro.tk/success.html', "_blank");
