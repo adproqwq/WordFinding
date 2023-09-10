0c4a5c68b75da38bc2f556b93c97c9158d12a9e8
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-09-10 13:21:05 +0800

    更新 index.js

--- js/index.js
+++ js/index.js
@@ -7,38 +7,38 @@ $(function () {
             }
             else{
                 if(url_value == 'homo'){
-                    const audioh = new Audio('../audio/homo.mp3')
+                    const audioh = new Audio('..\audio\homo.mp3')
                     audioh.play()
                 }
                 else if(url_value == 'rickroll'){
                     window.open('https://www.bilibili.com/video/BV1GJ411x7h7')
                 }
                 else if(url_value == 'spasmodic'){
-                    const audios = new Audio('../audio/spasmodic.mp3')
+                    const audios = new Audio('..\audio\spasmodic.mp3')
                     audios.play()
                 }
                 else if(url_value == 'chicken'){
-                    const audioc = new Audio('../audio/chicken.mp3')
+                    const audioc = new Audio('..\audio\chicken.mp3')
                     audioc.play()
                 }
                 else if(url_value == 'random'){
-                    const audior = new Audio('../audio/random.mp3')
+                    const audior = new Audio('..\audio\random.mp3')
                     audior.play()
                 }
                 else if(url_value == 'again'){
-                    const audioa = new Audio('../audio/again.mp3')
+                    const audioa = new Audio('..\audio\again.mp3')
                     audioa.play()
                 }
                 else if(url_value == 'one'){
-                    const audioo = new Audio('../audio/one.mp3')
+                    const audioo = new Audio('..\audio\one.mp3')
                     audioo.play()
                 }
                 else if(url_value == 'crime'){
-                    const audioi = new Audio('../audio/crime.mp3')
+                    const audioi = new Audio('..\audio\crime.mp3')
                     audioi.play()
                 }
                 else if(url_value == 'sky'){
-                    const audiok = new Audio('../audio/sky.mp3')
+                    const audiok = new Audio('..\audio\sky.mp3')
                     audiok.play()
                 }
                 else{
